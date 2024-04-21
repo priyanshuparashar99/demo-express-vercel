064315b9294f324be2f8e2dff4a23923efb7d786
Priyanshu Parashar <priyanshuparashar06@gmail.com>
2024-04-21 11:59:04 +0530

    New Route Created!

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get("/", (req, res) => {
   res.send(" Sujeet's server says, Hello!");
 });
 
+app.get("/me", (req, res) => {
+  res.json({
+    name: "Sujeet Gund",
+    currentStatus: "Student",
+  });
+});
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
